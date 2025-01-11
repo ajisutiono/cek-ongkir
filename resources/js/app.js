a05b52145aa988c2616b95f0ce8c7fdc7d33e591
Aji Sutiono <raisadajiwa@gmail.com>
2025-01-11 18:23:33 +0700

    implementation of select2 to search realtime

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,3 +20,39 @@ $('select[name="origin_province"]').on('change', function () {
         $('select[name="origin_city"]').empty();
     }
 });
+
+
+$(function () {
+    $('#destination_city').select2({
+        ajax: {
+            url: '/api/cities', 
+            type: "POST",       
+            dataType: 'json',   
+            delay: 150,         
+            data: function (params) {
+                return {
+                    _token: $('meta[name="csrf-token"]').attr('content'), 
+                    search: params.term ? params.term.trim() : '' 
+                };
+            },
+            processResults: function (response) {
+                return {
+                    results: response.map(function (item) {
+                        return {
+                            id: item.id,   
+                            text: item.title 
+                        };
+                    })
+                };
+            },
+            error: function (xhr, status, error) {
+                console.error('Error:', error); 
+                alert('Terjadi kesalahan saat memuat data. Silakan coba lagi.');
+            },
+            cache: true 
+        },
+        placeholder: "Pilih kota tujuan", 
+        minimumInputLength: 2
+    });
+});
+
