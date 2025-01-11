ad4d57b7a80cdb588add9104177123344f50c196
Aji Sutiono <raisadajiwa@gmail.com>
2025-01-11 07:22:56 +0700

    jQuery installation and displays city data using vite asset bundling

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -10,6 +11,10 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import $ from 'jquery'
+
+window.jQuery = window.$ = $
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
